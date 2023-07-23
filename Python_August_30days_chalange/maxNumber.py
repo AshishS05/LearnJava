45cb1dd8139462ea48d398eaaaeb1391d248089f
AshishS05 <ashish.pratap4321@gmail.com>
2023-07-23 22:44:35 +0530

    max number

---
+++ Python_August_30days_chalange/maxNumber.py
@@ -0,0 +1,14 @@
+a= int(input("Enter first number = "))
+b=int(input("Enter second number = "))
+
+if a>b :
+    print(a," is greater number")
+else :
+    print(b," is greater number")
+
+
+#or
+c=50
+d=100
+maximum = max(a,b,c,d)
+print(maximum," from mx functions")
